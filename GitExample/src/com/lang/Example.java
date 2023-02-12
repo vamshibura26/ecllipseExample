b8c933e7a82cc6b43ac47221de9e4aa9fefae821
vamsi <vamsi@10.0.0.35>
2023-02-11 21:26:48 -0500

    First commit
    uploading all files

---
+++ GitExample/src/com/lang/Example.java
@@ -0,0 +1,12 @@
+package com.lang;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Git Type");
+
+	}
+
+}
